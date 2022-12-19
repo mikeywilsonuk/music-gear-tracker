26e37f114c0e51ac974deb906364cbd80dbe1641
“Mikey <114588948+mikeywilsonuk@users.noreply.github.com>
2022-12-19 11:51:19 +0000

    Create html table and synch with SQL database

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 from flask import Flask , request , render_template
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
 
 import os
 if os.path.exists('data.db'):
    os.remove('data.db')
 
 # Configure the app
-app = Flask("db-app")
+app = Flask(__name__)
 
 # Connect to SQL db
 app.config['SQLALCHEMY_DATABASE_URI'] = r'sqlite://///Users/mikeywilson/coding/courses/cs50/project/data.db'
 db = SQLAlchemy(app)
 
+# Create SQL database and table
 class record(db.Model):
     name = db.Column(db.Text)
     age = db.Column(db.Integer)
     city = db.Column(db.Text)
     id = db.Column(db.Integer , primary_key=True)
-
 
     def __init__(self,n,a,c):
         self.name = n
@@ -26,12 +25,12 @@ class record(db.Model):
         self.city = c
 
 
-@app.route("/index")
+@app.route("/")
 def myform():
     data = render_template("index.html")
     return data
 
-
+# Add user input data from html form into SQL database
 @app.route("/add_data_sqlite")
 def add_data_sqlite():
     n = request.args.get("name")
@@ -42,4 +41,6 @@ def add_data_sqlite():
     obj = record(n ,a ,c)
     db.session.add(obj)
     db.session.commit()
-    return "successfull"
+    # Display SQL data in html form
+    records = record.query.all()
+    return render_template('index.html', records=records)
